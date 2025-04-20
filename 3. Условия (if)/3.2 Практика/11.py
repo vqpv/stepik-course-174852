7479eb3e8e45c8fb9cd537f06d06589b19876295
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-04-20 23:58:42 +0300

    3.2 - 11

---
+++ 3. Условия (if)/3.2 Практика/11.py
@@ -0,0 +1,6 @@
+n_1, n_2, n_3, n_4 = int(input()), int(input()), int(input()), int(input())
+
+if (n_1 == n_3 or n_2 == n_4) or (abs(n_1 - n_3) == abs(n_2 - n_4)):
+    print("YES")
+else:
+    print("NO")
