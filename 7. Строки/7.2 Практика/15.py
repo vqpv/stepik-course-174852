7ae65ab3d290602ee8720dbeb314189687f355a2
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-06-15 12:43:25 +0300

    7.2 - 15

---
+++ 7. Строки/7.2 Практика/15.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+s_1 = '   _~_    '
+s_2 = '  (o o)   '
+s_3 = ' /  V  \  '
+s_4 = '/(  _  )\ '
+s_5 = '  ^^ ^^   '
+
+print(s_1 * n)
+print(s_2 * n)
+print(s_3 * n)
+print(s_4 * n)
+print(s_5 * n)
